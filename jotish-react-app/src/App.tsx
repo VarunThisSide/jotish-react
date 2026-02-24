1a4498f8166eac19c01f508f2eeb88ae9e9987fd
VarunThisSide <varun50299@gmail.com>
2026-02-24 07:06:34 +0530

    few changes in ui for getting less lag

--- jotish-react-app/src/App.tsx
+++ jotish-react-app/src/App.tsx
@@ -1,5 +1,3 @@
-import { useEffect, useState } from "react";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import List from "./pages/List";
@@ -9,36 +7,18 @@ import PhotoResult from "./pages/PhotoResult";
 import Home from "./pages/Home";
 
 function App() {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-  useEffect(() => {
-    const handleMouseMove = (e: MouseEvent) => {
-      setPosition({ x: e.clientX, y: e.clientY });
-    };
 
-    window.addEventListener("mousemove", handleMouseMove);
-    return () => window.removeEventListener("mousemove", handleMouseMove);
-  }, []);
   return (
-    <>
-      <div 
-        className="custom-cursor" 
-        style={{ left: `${position.x}px`, top: `${position.y}px`, transform: 'translate(-50%, -50%)' }} 
-      />
-      <div 
-        className="cursor-glow" 
-        style={{ left: `${position.x}px`, top: `${position.y}px`, transform: 'translate(-50%, -50%)' }} 
-      />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/list" element={<List />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/visuals" element={<Visuals />} />
-          <Route path="/details" element={<Details />} />
-          <Route path="/photo-result" element={<PhotoResult />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/list" element={<List />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/visuals" element={<Visuals />} />
+        <Route path="/details" element={<Details />} />
+        <Route path="/photo-result" element={<PhotoResult />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
