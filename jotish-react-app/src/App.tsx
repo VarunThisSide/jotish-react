f99b6c1e2c8c6257af5b88b8ee893a986f6dc911
VarunThisSide <varun50299@gmail.com>
2026-02-24 06:59:19 +0530

    added modified cursor

--- jotish-react-app/src/App.tsx
+++ jotish-react-app/src/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import List from "./pages/List";
@@ -7,18 +9,36 @@ import PhotoResult from "./pages/PhotoResult";
 import Home from "./pages/Home";
 
 function App() {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+    };
 
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }, []);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/list" element={<List />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/visuals" element={<Visuals />} />
-        <Route path="/details" element={<Details />} />
-        <Route path="/photo-result" element={<PhotoResult />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <div 
+        className="custom-cursor" 
+        style={{ left: `${position.x}px`, top: `${position.y}px`, transform: 'translate(-50%, -50%)' }} 
+      />
+      <div 
+        className="cursor-glow" 
+        style={{ left: `${position.x}px`, top: `${position.y}px`, transform: 'translate(-50%, -50%)' }} 
+      />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/list" element={<List />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/visuals" element={<Visuals />} />
+          <Route path="/details" element={<Details />} />
+          <Route path="/photo-result" element={<PhotoResult />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
